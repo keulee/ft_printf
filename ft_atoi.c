7c430780172b67b75a2da6800e9258a4df535fe3
keulee <58826218+keulee@users.noreply.github.com>
2020-03-08 19:44:54 +0100

    ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keulee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/08 19:38:59 by keulee            #+#    #+#             */
+/*   Updated: 2020/03/08 19:40:52 by keulee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_atoi(const char *str)
+{
+	int					mark;
+	int					i;
+	unsigned long int	final_nbr;
+
+	i = 0;
+	mark = 1;
+	final_nbr = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\v' ||
+			str[i] == '\f' || str[i] == '\r' || str[i] == '\t')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i++] == '-')
+			mark = mark * -1;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		final_nbr = final_nbr * 10 + str[i] - '0';
+		i++;
+		if (final_nbr > 2147483647 && mark == 1)
+			return (-1);
+		if (final_nbr > 2147483648 && mark == -1)
+			return (0);
+	}
+	return (final_nbr * mark);
+}
