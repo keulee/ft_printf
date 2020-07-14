fa08dfc85bcba80f514a4e7e77f7359a68a536df
keulee <58826218+keulee@users.noreply.github.com>
2020-07-14 18:38:03 +0200

    Create ft_utoa_base_before.c

---
+++ hold/ft_utoa_base_before.c
@@ -0,0 +1,54 @@
+char	*ft_rev(char *string)
+{
+	int		len;
+	int		i;
+	char	tmp;
+	len = ft_strlen(string) - 1;
+	i = 0;
+	while (i < len)
+	{
+		tmp = string[i];
+		string[i] = string[len];
+		string[len] = tmp;
+		i++;
+		len--;
+	}
+	return (string);
+}
+unsigned int	ft_nb_len(unsigned long long *nb, int *div)
+{
+	unsigned int		i;
+	long long int		l_nb;
+	i = 0;
+	l_nb = *nb;
+	while (l_nb)
+	{
+		l_nb /= *div;
+		i++;
+	}
+	return (i);
+}
+char	*ft_utoa_base(unsigned long long nb, char *base)
+{
+	char		*string;
+	int			div;
+	int			i;
+	unsigned int nb_len;
+	div = ft_strlen(base);
+    i = 0;
+	nb_len = ft_nb_len(&nb, &div);
+	if (div != 16 || !(string = (char *)malloc(sizeof(char) * (nb_len + 1))))
+		return (NULL);
+    if (nb == 0)
+	{
+        string[0] = '0';
+		i = 1;
+	}
+	while (nb > 0)
+	{
+		string[i++] = base[nb % div];
+		nb = nb / div;
+	}
+	string[i] = '\0';
+	return (ft_rev(string));
+}
